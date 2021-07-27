3f7d15f305b95088b7253a8d852a58bddf7c43f6
Udalov Max <re.udalov@gmail.com>
2021-07-27 03:03:25 +0300

    add cron package with tests

---
+++ cron/cron.go
@@ -0,0 +1,259 @@
+package cron
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+)
+
+// Expression represents a standard crontab expression and a command to
+// execute.
+//
+// Misc:
+// See https://en.wikipedia.org/wiki/Cron#CRON_expression for valid fields
+// format.
+type Expression struct {
+	Minutes   []uint8
+	Hours     []uint8
+	MonthDays []uint8
+	Months    []uint8
+	WeekDays  []uint8
+	Command   string
+}
+
+// number of args in expression
+//
+// minutes hours monthdays months weekdays `command [args]`
+const numExressionArgs = 6
+
+// DumpFormatted pretty-prints expression to w.
+func (e *Expression) DumpFormatted(w io.Writer) error {
+	tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', tabwriter.TabIndent)
+	defer func() error {
+		if err := tw.Flush(); err != nil {
+			return fmt.Errorf("could not flush writer: %w", err)
+		}
+		return nil
+	}()
+
+	rows := []struct {
+		name  string
+		value string
+	}{
+		{
+			name:  "minute",
+			value: join(e.Minutes),
+		},
+		{
+			name:  "hour",
+			value: join(e.Hours),
+		},
+		{
+			name:  "day of month",
+			value: join(e.MonthDays),
+		},
+		{
+			name:  "month",
+			value: join(e.Months),
+		},
+		{
+			name:  "day of week",
+			value: join(e.WeekDays),
+		},
+		{
+			name:  "command",
+			value: e.Command,
+		},
+	}
+
+	for _, row := range rows {
+		if _, err := fmt.Fprintf(tw, "%s\t%s\n", row.name, row.value); err != nil {
+			return fmt.Errorf("could not write %s: %w", row.name, err)
+		}
+	}
+
+	return nil
+}
+
+func join(ss []uint8) string {
+	sb := strings.Builder{}
+	for i, s := range ss {
+		if i > 0 {
+			sb.WriteByte(' ')
+		}
+		sb.WriteString(strconv.Itoa(int(s)))
+	}
+
+	return sb.String()
+}
+
+// ParseExpression parses line in a valid cron expression.
+func ParseExpression(line string) (e Expression, err error) {
+	args := strings.SplitN(line, " ", numExressionArgs)
+
+	if e.Minutes, err = parseTime(args[0], 0, 59); err != nil {
+		return e, fmt.Errorf("invalid minutes arg: %w", err)
+	}
+
+	if e.Hours, err = parseTime(args[1], 0, 23); err != nil {
+		return e, fmt.Errorf("invalid hours arg: %w", err)
+	}
+
+	if e.MonthDays, err = parseTime(args[2], 1, 31); err != nil {
+		return e, fmt.Errorf("invalid monthdays arg: %w", err)
+	}
+
+	if e.Months, err = parseTime(args[3], 1, 12); err != nil {
+		return e, fmt.Errorf("invalid month arg: %w", err)
+	}
+
+	if e.WeekDays, err = parseTime(args[4], 0, 6); err != nil {
+		return e, fmt.Errorf("invalid weekdays arg: %w", err)
+	}
+
+	if args[5] == "" {
+		return e, errors.New("expected command but got an empty string")
+	}
+
+	e.Command = args[5]
+
+	return e, nil
+}
+
+func parseTime(arg string, min, max uint8) ([]uint8, error) {
+	if arg == "" {
+		return nil, errors.New("expected arg but got empty string")
+	}
+
+	if arg == "*" {
+		result := make([]uint8, 0, max-min+1)
+		for i := min; i <= max; i++ {
+			result = append(result, i)
+		}
+
+		return result, nil
+	}
+
+	rnge := strings.Split(arg, "-")
+	if len(rnge) > 1 {
+		return parseRange(rnge, min, max)
+	}
+
+	steps := strings.Split(arg, ",")
+	if len(steps) > 1 {
+		return parseSteps(steps, min, max)
+	}
+
+	intervals := strings.Split(arg, "/")
+	if len(intervals) > 1 {
+		return parseIntervals(intervals, min, max)
+	}
+
+	exact, err := parseIntegral(arg, min, max)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse exact value: %w", err)
+	}
+
+	return []uint8{exact}, nil
+}
+
+func parseIntegral(s string, min, max uint8) (uint8, error) {
+	v, err := strconv.ParseUint(s, 10, 8)
+	if err != nil {
+		return 0, fmt.Errorf("could not parse value: %w", err)
+	}
+
+	if uint8(v) < min || uint8(v) > max {
+		return 0, fmt.Errorf("value must be in range [%d; %d]", min, max)
+	}
+
+	return uint8(v), nil
+}
+
+func parseRange(rnge []string, min, max uint8) ([]uint8, error) {
+	if len(rnge) != 2 {
+		return nil, fmt.Errorf("expected range but got %+v", rnge)
+	}
+
+	first, err := parseIntegral(rnge[0], min, max)
+	if err != nil {
+		return nil, fmt.Errorf("invalid begin of range %q: %w", rnge[0], err)
+	}
+
+	end, err := parseIntegral(rnge[1], min, max)
+	if err != nil {
+		return nil, fmt.Errorf("invalid end of range %q: %w", rnge[1], err)
+	}
+
+	if first >= end {
+		return nil, fmt.Errorf("invalid range [%d; %d]", first, end)
+	}
+
+	result := make([]uint8, 0, end-first)
+	for i := first; i <= end; i++ {
+		result = append(result, i)
+	}
+
+	return result, nil
+}
+
+func parseSteps(steps []string, min, max uint8) ([]uint8, error) {
+	result := make([]uint8, 0, len(steps))
+
+	unique := make(map[uint8]struct{}, len(steps))
+	for _, s := range steps {
+		if s == "" {
+			return nil, errors.New("expected step but got an empty string")
+		}
+
+		step, err := parseIntegral(s, min, max)
+		if err != nil {
+			return nil, fmt.Errorf("invalid step %q: %w", s, err)
+		}
+
+		if _, ok := unique[step]; !ok {
+			unique[step] = struct{}{}
+			result = append(result, step)
+		}
+	}
+
+	return result, nil
+}
+
+func parseIntervals(intervals []string, min, max uint8) ([]uint8, error) {
+	if len(intervals) != 2 {
+		return nil, errors.New("malformed intervals arg")
+	}
+
+	start := uint8(0)
+	if intervals[0] != "*" {
+		s, err := parseIntegral(intervals[0], min, max)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse starting point: %w", err)
+		}
+		start = s
+	}
+
+	every, err := parseIntegral(intervals[1], min, max)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse repeated interval: %w", err)
+	}
+
+	if every == 0 {
+		return nil, errors.New("repeated interval must be greater than 0")
+	}
+
+	if (max+1)%every != 0 {
+		return nil, fmt.Errorf("invalid repeated interval: %d for %d", every, max+1)
+	}
+
+	result := make([]uint8, 0, (max+1)/every)
+	for i := start + min; i < max; i = i + every {
+		result = append(result, i)
+	}
+
+	return result, nil
+}
---
+++ cron/cron_test.go
@@ -0,0 +1,309 @@
+package cron
+
+import (
+	"testing"
+)
+
+func TestParseIntegral(t *testing.T) {
+	cases := []struct {
+		name               string
+		value              string
+		min, max, expected uint8
+		shouldFail         bool
+	}{
+		{
+			name:       "success",
+			value:      "10",
+			min:        0,
+			max:        11,
+			expected:   10,
+			shouldFail: false,
+		},
+		{
+			name:       "invalid value",
+			value:      "-1",
+			shouldFail: true,
+		},
+		{
+			name:       "below min",
+			value:      "1",
+			min:        2,
+			shouldFail: true,
+		},
+		{
+			name:       "above max",
+			value:      "10",
+			min:        2,
+			max:        3,
+			shouldFail: true,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			v, err := parseIntegral(c.value, c.min, c.max)
+			if err != nil {
+				if c.shouldFail {
+					return
+				}
+				t.Fatalf("parse of %q failed: %v", c.value, err)
+			}
+
+			if c.expected != v {
+				t.Fatalf("parse mismatch: want %d; got %d", c.expected, v)
+			}
+		})
+	}
+}
+
+func TestParseRange(t *testing.T) {
+	cases := []struct {
+		name       string
+		value      []string
+		rnge       []uint8
+		min, max   uint8
+		shouldFail bool
+	}{
+		{
+			name:       "empty range",
+			value:      nil,
+			shouldFail: true,
+		},
+		{
+			name:       "invalid range (too small)",
+			value:      []string{"1"},
+			shouldFail: true,
+		},
+		{
+			name:       "invalid range (too big)",
+			value:      []string{"1", "2", "3"},
+			shouldFail: true,
+		},
+		{
+			name:       "begin less than min",
+			value:      []string{"1", "10"},
+			min:        5,
+			max:        15,
+			shouldFail: true,
+		},
+		{
+			name:       "begin more than max",
+			value:      []string{"10", "100"},
+			min:        1,
+			max:        10,
+			shouldFail: true,
+		},
+		{
+			name:       "end less than min",
+			value:      []string{"1", "0"},
+			min:        0,
+			max:        15,
+			shouldFail: true,
+		},
+		{
+			name:       "end more than max",
+			value:      []string{"10", "100"},
+			min:        1,
+			max:        90,
+			shouldFail: true,
+		},
+		{
+			name:       "begin is more than end",
+			value:      []string{"100", "10"},
+			min:        0,
+			max:        100,
+			shouldFail: true,
+		},
+		{
+			name:       "happy path",
+			value:      []string{"1", "10"},
+			min:        0,
+			max:        59,
+			rnge:       []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			shouldFail: false,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			rnge, err := parseRange(c.value, c.min, c.max)
+			if err != nil {
+				if c.shouldFail {
+					return
+				}
+				t.Fatalf("could not parse range: %v", err)
+			}
+
+			if len(c.rnge) != len(rnge) {
+				t.Fatalf("len mismatch: want %+v; got %+v", c.rnge, rnge)
+			}
+
+			for i := range c.rnge {
+				want := c.rnge[i]
+				got := rnge[i]
+
+				if want != got {
+					t.Errorf("mismatch at %d: want %d; got %d", i, want, got)
+				}
+			}
+		})
+	}
+}
+
+func TestParseSteps(t *testing.T) {
+	cases := []struct {
+		name       string
+		steps      []string
+		min, max   uint8
+		expected   []uint8
+		shouldFail bool
+	}{
+		{
+			name:       "has empty step",
+			steps:      []string{"", "1", "2"},
+			shouldFail: true,
+		},
+		{
+			name:       "has invalid step",
+			steps:      []string{"3", "1", "4", "5"},
+			min:        2,
+			max:        10,
+			shouldFail: true,
+		},
+		{
+			name:       "has invalid step (non integer)",
+			steps:      []string{"3", "hello", "4", "5"},
+			min:        2,
+			max:        10,
+			shouldFail: true,
+		},
+		{
+			name:       "non unique steps",
+			steps:      []string{"1", "1", "1", "1"},
+			min:        0,
+			max:        10,
+			expected:   []uint8{1},
+			shouldFail: false,
+		},
+		{
+			name:       "valid steps",
+			steps:      []string{"1", "2", "3", "4"},
+			min:        0,
+			max:        10,
+			expected:   []uint8{1, 2, 3, 4},
+			shouldFail: false,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			actual, err := parseSteps(c.steps, c.min, c.max)
+			if err != nil {
+				if c.shouldFail {
+					return
+				}
+				t.Fatalf("could not parse steps: %v", err)
+			}
+
+			if len(c.expected) != len(actual) {
+				t.Fatalf("len mismatch: want %+v; got %+v", c.expected, actual)
+			}
+
+			for i := range c.expected {
+				want := c.expected[i]
+				got := actual[i]
+
+				if want != got {
+					t.Errorf("mismatch at %d: want %d; got %d", i, want, got)
+				}
+			}
+		})
+	}
+}
+
+func TestParseTime(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    string
+		min, max uint8
+		expected []uint8
+	}{
+		{
+			name:     "parse range minutes",
+			input:    "1-10",
+			min:      0,
+			max:      59,
+			expected: []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:     "parse exact minutes",
+			input:    "1",
+			min:      0,
+			max:      59,
+			expected: []uint8{1},
+		},
+		{
+			name:  "parse every hour",
+			input: "*",
+			min:   0,
+			max:   23,
+			expected: []uint8{
+				0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+				13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+			},
+		},
+		{
+			name:  "parse step minutes",
+			input: "15,30,45,00",
+			min:   0,
+			max:   59,
+			expected: []uint8{
+				15, 30, 45, 00,
+			},
+		},
+		{
+			name:  "parse every 15 minutes",
+			input: "*/15",
+			min:   0,
+			max:   59,
+			expected: []uint8{
+				0, 15, 30, 45,
+			},
+		},
+		{
+			name:  "parse every 15 minutes from 1",
+			input: "1/15",
+			min:   0,
+			max:   59,
+			expected: []uint8{
+				1, 16, 31, 46,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			actual, err := parseTime(c.input, c.min, c.max)
+			if err != nil {
+				t.Fatalf("could not parse expression: %v", err)
+			}
+			if len(c.expected) != len(actual) {
+				t.Fatalf("len mismatch: want %+v; got %+v", c.expected, actual)
+			}
+
+			for i := range c.expected {
+				want := c.expected[i]
+				got := actual[i]
+
+				if want != got {
+					t.Errorf("mismatch at %d: want %d; got %d", i, want, got)
+				}
+			}
+
+		})
+	}
+}
